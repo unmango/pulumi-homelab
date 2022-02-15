27503517bad70580bb45d4980ea45b29d889e521
Erik Rasmussen <ecr.referee@hotmail.com>
2022-02-14 23:13:52 -0600

    Re-generate sdks with current changes

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,40 +8,52 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
+VERSION = "0.0.0"
+PLUGIN_VERSION = "0.0.0"
+
 class InstallPluginCommand(install):
     def run(self):
         install.run(self)
         try:
-            check_call(['pulumi', 'plugin', 'install', 'resource', 'homelab', '${PLUGIN_VERSION}'])
+            check_call(['pulumi', 'plugin', 'install', 'resource', 'homelab', PLUGIN_VERSION])
         except OSError as error:
             if error.errno == errno.ENOENT:
-                print("""
+                print(f"""
                 There was an error installing the homelab resource provider plugin.
                 It looks like `pulumi` is not installed on your system.
                 Please visit https://pulumi.com/ to install the Pulumi CLI.
                 You may try manually installing the plugin by running
-                `pulumi plugin install resource homelab ${PLUGIN_VERSION}`
+                `pulumi plugin install resource homelab {PLUGIN_VERSION}`
                 """)
             else:
                 raise
 
 
 def readme():
-    with open('README.md', encoding='utf-8') as f:
-        return f.read()
+    try:
+        with open('README.md', encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        return "homelab Pulumi Package - Development Version"
 
 
 setup(name='pulumi_homelab',
-      version='${VERSION}',
+      version=VERSION,
+      description="Popular homelab applications for docker and kubernetes",
       long_description=readme(),
       long_description_content_type='text/markdown',
       cmdclass={
           'install': InstallPluginCommand,
       },
+      keywords='category/cloud category/infrastructure kind/component pulumi kubernetes k8s docker homelab',
+      project_urls={
+          'Repository': 'https://github.com/unmango/pulumi-homelab'
+      },
       packages=find_packages(),
       package_data={
           'pulumi_homelab': [
               'py.typed',
+              'pulumi-plugin.json',
           ]
       },
       install_requires=[
