483b3f3c704a1557e6ffed959e8a5e1c144a4b3a
Erik Rasmussen <ecr.referee@hotmail.com>
2022-03-05 15:50:08 -0600

    Remove args from example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,7 +1,5 @@
 import * as homelab from "@unmango/pulumi-homelab";
 
-const heimdall = new homelab.docker.linuxserver.Heimdall("heimdall", {
-    configPath: './config'
-});
+const heimdall = new homelab.docker.linuxserver.Heimdall("heimdall");
 
 export const container = heimdall.container;
